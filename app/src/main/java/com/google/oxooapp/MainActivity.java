0da36219af60e86cba3138675d2538dc2cc45386
Hijbullah Al Mahmud <hijbullahmahmud14@gmail.com>
2020-04-27 13:56:28 +0600

    bug solved

--- app/src/main/java/com/google/oxooapp/MainActivity.java
+++ app/src/main/java/com/google/oxooapp/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends Activity {
 
     @Override
     public String customView() {
-        return "bd80dd1f-0715-4c83-aa83-d0ae07459742_0";
+        return "bd80dd1f-0715-4c83-aa83-d0ae07459742";
     }
 
     @Override
